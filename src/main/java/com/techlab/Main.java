32e0ea26b8805f83dd51acce7f680d23d828f246
daveSuter <daveysuter@gmail.com>
2025-05-25 17:31:48 -0300

    Se agrega aclaración en clase Main

--- src/main/java/com/techlab/Main.java
+++ src/main/java/com/techlab/Main.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//Tengo experiencia previa, aunque más que nada trabajo con Spring y la verdad me enrosque bastante con esto.
+//Cualquier consulta estoy a dispocisión.
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
